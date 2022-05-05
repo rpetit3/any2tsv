738871450f751def678913533f31b85f0118dc9c
Debian <rpetit3@vm-rp3-training.p4sb4yz3tjkenien1wv3nq2hca.gx.internal.cloudapp.net>
2022-05-05 03:53:13 +0000

    start setting up directory structure

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = "0.0.1"
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="any2tsv",
+    version=version,
+    description="Convert various bioinformatic outputs to TSV",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    keywords=[
+        "parser",
+        "bioinformatics",
+        "tools",
+        "biology",
+        "sequencing",
+        "NGS",
+        "next generation sequencing",
+        "bactopia"
+    ],
+    author="Robert A. Petit III",
+    author_email="robbie.petit@gmail.com",
+    url="https://github.com/rpetit3/any2tsv",
+    license="MIT",
+    entry_points={"console_scripts": ["any2tsv=any2tsv.__main__:run_any2tsv"]},
+    install_requires=required,
+    packages=find_packages(exclude=("docs")),
+    include_package_data=True,
+    zip_safe=False,
+)
